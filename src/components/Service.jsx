8ae797c72d80228b3bdc1e2ee09451788ec8ec07
KarolWoz <111453950+KarolWoz@users.noreply.github.com>
2023-02-10 18:00:43 +0100

    last adjustments

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,8 +1,9 @@
 const Service = ({ name, isNew }) => {
   const className = isNew ? "service first-one" : "service";
+
   return (
     <div className={className}>
-      <div></div>
+      <div>{isNew}</div>
       <p className="serv-description">
         {name} {isNew && <span>(nowość)</span>}
       </p>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,13 +1,5 @@
 import Service from "./Service";
-
-const offers = [
-  { name: "Usługa 1", isNew: true },
-  { name: "Usługa 2", isNew: false },
-  { name: "Usługa 3", isNew: false },
-  { name: "Usługa 4", isNew: false },
-  { name: "Usługa 5", isNew: false },
-  { name: "Usługa 6", isNew: false },
-];
+import { offers } from "../Data/Offers.js";
 
 const Services = () => {
   return (
